61d55f0a0a1484756e7c8b2a811bbdf1c79a046a
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-02-03 19:16:02 -0500

    updated configuration for database properties and test profile

--- src/main/java/com/orion/trivia/TriviaApplication.java
+++ src/main/java/com/orion/trivia/TriviaApplication.java
@@ -2,6 +2,7 @@ package com.orion.trivia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
--- src/main/java/com/orion/trivia/entity/Question.java
+++ src/main/java/com/orion/trivia/entity/Question.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
@@ -12,7 +15,8 @@ import java.util.Date;
 @AllArgsConstructor
 @Data
 public class Question {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer question_num;
     private Date timestamp;
 }
